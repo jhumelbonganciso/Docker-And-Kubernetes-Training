cc0b14670c027ce540019a64d14e41e53590ece2
jhumelbonganciso <jhumelbonganciso04@gmail.com>
2023-04-08 01:53:42 +0800

    First docker deployment of java application

---
+++ Java-test/Readme.md
@@ -0,0 +1,8 @@
+## Instruction.
+1. Pull the repo.
+2. Run docker build .
+3. Copy the signature.
+4. Run 
+```
+docker run -it {image signature}
+```
---
+++ Java-test/Test.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int choice = 10;
+        String name = "";
+        
+        System.out.print("Please enter your name => ");
+        name = s.nextLine();
+        while (choice != 2) {
+            System.out.println("1. Say Hi");
+            System.out.println("2. Exit the app");
+            System.out.print("Choice => ");
+            choice = s.nextInt();
+
+            switch(choice){
+                case 1: System.out.println("Hi, " + name);
+                break;
+                case 2: System.out.println("Bye!");
+
+            }
+        }
+    }
+}
